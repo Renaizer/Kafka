5653a9f2bffa508637bf8525a63d27879bf06633
Renaizer <hamidsheaven@gmail.com>
2020-01-13 09:36:17 +0530

    Kafka API consumer added.

---
+++ App/src/main/java/simpleConsumer/Consumer.java
@@ -0,0 +1,50 @@
+package simpleConsumer;
+
+import java.time.Duration;
+import java.util.*;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+
+        String topicName = "sea_vessel_position_reports";
+        
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "PLAINTEXT://127.0.0.1:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer  = null;
+        Duration pollingDuration = Duration.ofMillis(1000);
+        
+        try {
+	        consumer = new KafkaConsumer<>(props);
+	        consumer.subscribe(Arrays.asList(topicName));
+	        
+	//        ConsumerRecords<String, String> records1 = consumer.poll(10);
+	        
+	        while (true){
+	            ConsumerRecords<String, String> records1 = consumer.poll(pollingDuration);
+	            System.out.println("Number of msg recieved = "+records1.count());
+	            for (ConsumerRecord<String, String> record: records1) {
+	                System.out.println("Supplier id= " + record.toString());
+	            }
+	        }
+        }
+        catch (Exception e) {
+
+        }
+        finally {
+        	consumer.close();
+		}
+    }
+}
+    
+
+
